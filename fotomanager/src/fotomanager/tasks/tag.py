328170e2609c2cb14b2eee226d0e88137cf1aabf
Ashish Ranjan Vidyarthi <ashish.vid@gmail.com>
2021-02-13 21:07:17 -0800

    Support sapces in image name

--- fotomanager/src/fotomanager/tasks/tag.py
+++ fotomanager/src/fotomanager/tasks/tag.py
@@ -14,7 +14,7 @@ def update_copyright(src, app_config):
     OPT_ARTIST = r'-artist="{}" '
     OPT_NOTICE = r"-d %Y -IPTC:CopyrightNotice'<© $createdate {}' "
     OPT_COPYRIGHT = r"-Copyright'<© $CreateDate {}' "
-    OPT_USAGE = r'-usageterms="{}" -r -P -progress -overwrite_original {}'
+    OPT_USAGE = r'-usageterms="{}" -r -P -progress -overwrite_original "{}"'
     cmd_all = TOOL_CMD.format(app_config['BIN']['EXIFTOOL']) + \
                 OPT_ARTIST.format(artist) + \
                 OPT_NOTICE.format(notice) + \
@@ -32,7 +32,7 @@ def remove_tags_from_flickr(src, app_config):
     OPT_LENSSNUM = r'-LensSerialNumber= '
     OPT_ISNUM = r'-InternalSerialNumber= '
     OPT_CID = r'-CameraID= '
-    SRC = "-P -overwrite_original {}".format(src)
+    SRC = r'-P -overwrite_original "{}"'.format(src)
     cmd_all = TOOL_CMD.format(app_config['BIN']['EXIFTOOL']) + \
         OPT_SOFTWARE + OPT_SNUM + OPT_LENSSNUM + \
         OPT_ISNUM + OPT_CID + SRC
